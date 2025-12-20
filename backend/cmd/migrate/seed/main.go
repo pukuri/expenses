a77f3bfd4098dabaf40c55b576b76914e924a6ce
Fachri Akbar Rahmanda <fachriakbarrahmanda@gmail.com>
2025-12-20 11:28:51 +0700

    debug logs

--- backend/cmd/migrate/seed/main.go
+++ backend/cmd/migrate/seed/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 
 	"github.com/pukuri/expenses/config"
@@ -17,13 +16,14 @@ func main() {
 		log.Fatal("cannot load config:", err)
 	}
 
-	fmt.Printf("DB_USER='%s'\n", cfg.DB.User)
-	fmt.Printf("DB_PASSWORD='%s'\n", cfg.DB.Password)
-	fmt.Printf("DB_NAME='%s'\n", cfg.DB.Name)
-	fmt.Printf("INSTANCE_CONNECTION_NAME='%s'\n", cfg.DB.InstanceConnectionName)
-
 	conn, err := db.New(cfg)
 	if err != nil {
+		log.Printf("DB_USER='%s'\n", cfg.DB.User)
+		log.Printf("DB_PASSWORD='%s'\n", cfg.DB.Password)
+		log.Printf("DB_NAME='%s'\n", cfg.DB.Name)
+		log.Printf("INSTANCE_CONNECTION_NAME='%s'\n", cfg.DB.InstanceConnectionName)
+
+		log.Printf("SQL.OPEN ERROR: %v\n", err)
 		log.Fatal(err)
 	}
 
