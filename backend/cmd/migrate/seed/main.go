03707bd853f9d2a4cefc48dceea5d2b86f9b2691
Fachri Akbar Rahmanda <fachriakbarrahmanda@gmail.com>
2025-12-20 11:16:46 +0700

    add logs

--- backend/cmd/migrate/seed/main.go
+++ backend/cmd/migrate/seed/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/pukuri/expenses/config"
@@ -15,6 +16,11 @@ func main() {
 	if err != nil {
 		log.Fatal("cannot load config:", err)
 	}
+
+	fmt.Printf("DB_USER='%s'\n", cfg.DB.User)
+	fmt.Printf("DB_PASSWORD='%s'\n", cfg.DB.Password)
+	fmt.Printf("DB_NAME='%s'\n", cfg.DB.Name)
+	fmt.Printf("INSTANCE_CONNECTION_NAME='%s'\n", cfg.DB.InstanceConnectionName)
 
 	conn, err := db.New(cfg)
 	if err != nil {
