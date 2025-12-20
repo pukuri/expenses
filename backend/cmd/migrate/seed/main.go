6af2526a6b97a1fa96de796180b0d014de8892e6
Fachri Akbar Rahmanda <fachriakbarrahmanda@gmail.com>
2025-12-20 13:36:54 +0700

    adjust logs

--- backend/cmd/migrate/seed/main.go
+++ backend/cmd/migrate/seed/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"log"
+	"fmt"
 
 	"github.com/pukuri/expenses/config"
 	"github.com/pukuri/expenses/internal/db"
@@ -13,17 +13,19 @@ import (
 func main() {
 	cfg, err := config.Load()
 	if err != nil {
-		log.Fatal("cannot load config:", err)
+		// fmt.Fatal("cannot load config:", err)
+		fmt.Printf("cannot load config:", err)
+		return
 	}
 
 	conn, err := db.New(cfg)
 	if err != nil {
-		log.Printf("DB_USER='%s'\n", cfg.DB.User)
-		log.Printf("DB_PASSWORD='%s'\n", cfg.DB.Password)
-		log.Printf("DB_NAME='%s'\n", cfg.DB.Name)
-		log.Printf("INSTANCE_CONNECTION_NAME='%s'\n", cfg.DB.InstanceConnectionName)
+		fmt.Printf("DB_USER='%s'\n", cfg.DB.User)
+		fmt.Printf("DB_PASSWORD='%s'\n", cfg.DB.Password)
+		fmt.Printf("DB_NAME='%s'\n", cfg.DB.Name)
+		fmt.Printf("INSTANCE_CONNECTION_NAME='%s'\n", cfg.DB.InstanceConnectionName)
 
-		log.Printf("SQL.OPEN ERROR: %v\n", err)
+		fmt.Printf("SQL.OPEN ERROR: %v\n", err)
 		return
 	}
 
